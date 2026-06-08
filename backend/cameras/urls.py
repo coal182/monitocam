da945d061246650361a5b9effc189fdf34dd5581
Cristian Martín <coal182@gmail.com>
2026-06-08 13:14:55 +0200

    feat: add cameras URLs with DRF router

---
+++ backend/cameras/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cameras.views import CameraViewSet
+
+router = DefaultRouter()
+router.register(r"cameras", CameraViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
